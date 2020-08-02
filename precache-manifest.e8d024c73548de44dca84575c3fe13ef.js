1f122e465fb0f6bd5a9320f0d3be2146df640ba4
Shalu Kumari <shaluraics22@gmail.com>
2020-08-02 10:31:19 +0530

    Updates

---
+++ precache-manifest.e8d024c73548de44dca84575c3fe13ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c34f2ba0e09be8ffcc0bbcb1880dd3d",
+    "url": "/activity-tracker/index.html"
+  },
+  {
+    "revision": "875e052f09ac0dd622b5",
+    "url": "/activity-tracker/static/css/2.25ff2e5b.chunk.css"
+  },
+  {
+    "revision": "dde3101e25e324b415c9",
+    "url": "/activity-tracker/static/css/main.dc9cc4cf.chunk.css"
+  },
+  {
+    "revision": "875e052f09ac0dd622b5",
+    "url": "/activity-tracker/static/js/2.54f987e4.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/activity-tracker/static/js/2.54f987e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dde3101e25e324b415c9",
+    "url": "/activity-tracker/static/js/main.06b5c621.chunk.js"
+  },
+  {
+    "revision": "66dcc5d305bb3922622e",
+    "url": "/activity-tracker/static/js/runtime-main.ef064ab4.js"
+  }
+]);
